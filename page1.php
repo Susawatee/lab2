5826d7bb427d7e374c9dc0baf0e94d930096bea3
Susawatee <106943921+Susawatee@users.noreply.github.com>
2023-01-20 21:45:11 +0700

    Update page1.php

--- page1.php
+++ page1.php
@@ -2,11 +2,22 @@
 <html>
 
 <body>
-    <form action="show.php" method="post">
-        Name:<input type="text" name="name"><br>
-        Email:<input type="text" name="email"><br>
-        <input type="submit" value="send">
-</form>
-</body>
 
-</html>
+    <h1>รับข้อมูลและแสดงผล</h1>
+    <form action="show.php" method="get" style="margin-left: 80px;">
+        ชื่อ:<input type="text" name="name"><br>
+        การุณาระบุเพศ:
+        <select name="gender">
+            <option value="male">หญิง</option>
+            <option value="female">ชาย</option>
+        </select><br>
+        การุณาใส่อายุ:
+        <input type="number" name="num" style="width: 40px;"><br>
+        สถานะ:
+        <select name="status">
+            <option value="single">โสด</option>
+            <option value="marry">แต่งงานแล้ว</option>
+        </select><br>
+        <input type="submit" value="ส่ง">
+    </form>
+</body>
